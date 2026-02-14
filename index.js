cd63463b8f7e8636e129cf2ab36a3eb5fc0480e4
Marc Ferguson <marc@fergytech.com>
2026-02-14 17:31:14 -0500

    fix: correct package name and version in package-lock.json docs: update example import path in getDatabasePath documentation

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
  *
  * @example
  * ```javascript
- * import { getDatabasePath } from '@tabletop-android/airport-db-data-sqlite';
+ * import { getDatabasePath } from '@tabletopandroid/airport-db-data-sqlite';
  *
  * const dbPath = getDatabasePath();
- * console.log(dbPath); // /path/to/node_modules/@tabletop-android/airport-db-data-sqlite/data/airports.sqlite
+ * console.log(dbPath); // /path/to/node_modules/@tabletopandroid/airport-db-data-sqlite/data/airports.sqlite
  * ```
  */
 export function getDatabasePath() {
